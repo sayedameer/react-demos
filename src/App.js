e132640baf3e4bbba8349797395a01c8ac861357
sayedameer <iamawesome701@gmail.com>
2021-04-08 00:24:23 +1000

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,50 +1,60 @@
 import React from "react";
-
-const users = [
-  { id: "a", name: "Robin" },
-  { id: "b", name: "Dennis" }
-];
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
+  const [users, setUsers] = React.useState([
+    { id: "a", name: "Robin" },
+    { id: "b", name: "Dennis" }
+  ]);
+
   const [text, setText] = React.useState("");
-  const [search, setSearch] = React.useState("");
 
   const handleText = (event) => {
     setText(event.target.value);
   };
 
-  const handleSearch = () => {
-    setSearch(text);
+  const handleAddUser = () => {
+    setUsers(users.concat({ id: uuidv4(), name: text }));
   };
 
-  const filteredUsers = users.filter((user) => {
-    return user.name.toLowerCase().includes(search.toLowerCase());
-  });
+  const handleRemove = React.useCallback(
+    (id) => {
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
 
   return (
     <div>
       <input type="text" value={text} onChange={handleText} />
-      <button type="button" onClick={handleSearch}>
-        Search
+      <button type="button" onClick={handleAddUser}>
+        Add User
       </button>
 
-      <List list={filteredUsers} />
+      <List list={users} onRemove={handleRemove} />
     </div>
   );
 };
 
-const List = ({ list }) => {
+const List = ({ list, onRemove }) => {
   return (
     <ul>
       {list.map((item) => (
-        <ListItem key={item.id} item={item} />
+        <ListItem key={item.id} item={item} onRemove={onRemove} />
       ))}
     </ul>
   );
 };
 
-const ListItem = ({ item }) => {
-  return <li>{item.name}</li>;
+const ListItem = ({ item, onRemove }) => {
+  return (
+    <li>
+      {item.name}
+      <button type="button" onClick={() => onRemove(item.id)}>
+        Remove
+      </button>
+    </li>
+  );
 };
 
 export default App;
